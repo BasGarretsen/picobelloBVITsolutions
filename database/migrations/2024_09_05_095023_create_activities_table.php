487614c0ce76c7138da014f9ac2e4c0661ed3465
remcod <remco.dellemann@student.graafschapcollege.nl>
2024-09-11 13:23:33 +0200

    Model ,controller en migration voor activities

---
+++ database/migrations/2024_09_05_095023_create_activities_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('activity name');
+            $table->string('location');
+            $table->boolean('including_food');
+            $table->string('omschrijving');
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
+            $table->double('kosten');
+            $table->integer('maximum_number_of_participants');
+            $table->integer('minimum_number of_participants');
+            $table->string('image');
+            $table->json('supplies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
