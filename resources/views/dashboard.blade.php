1382e02518fc98157e1ae69dd38e9fe16a138a24
fridtpup <fridtjof.graaff@student.graafschapcollege.nl>
2024-09-19 11:03:31 +0200

    fixed center styling in dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,8 +5,8 @@
 <h1 class="font-bold text-4xl">Welcome {{ Auth::user()->name }}</h1>
 
 
-<div style=" display: flex; flex-direction: column; align-items: center;">
-    <div class="buttonDiv" >
+<div style="display: flex; flex-direction: column; align-items: center;">
+    <div class="buttonDiv">
         <a href="/create_activity">
             <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
                             Create Activity
