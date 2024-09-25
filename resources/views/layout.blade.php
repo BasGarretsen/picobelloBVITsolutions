6f8330d3aaa5a093623de2d1cd8f9079f527bc55
Steyntjeh <steyn.ruesink@gmail.com>
2024-09-25 13:51:30 +0200

    footer weg

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -48,31 +48,6 @@
         @yield('content')
         
         @yield('sidebar')
-        <footer class="bg-[#f5af00]">
-            <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                <div class="sm:flex sm:items-center sm:justify-between">
-                    <a href="/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="https://s3.eu-central-1.amazonaws.com/covadis/media/Logos/logo_covadis_2016.png" class="h-8" alt="Flowbite Logo" />
-                    </a>
-                    <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0">
-                        <li>
-                            <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                <hr class="my-6 sm:mx-auto lg:my-8" />
-                <span class="block text-sm text-white sm:text-center">© 2023 <a href="https://covadis.nl/" class="hover:underline">Covadis™</a>. All Rights Reserved.</span>
-            </div>
-    </footer>
 </body>
 
 </html>
