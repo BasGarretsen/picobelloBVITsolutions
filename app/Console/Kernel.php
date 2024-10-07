994e35d9a8e8c553cb4aee31d21cb0ca401a2176
Steyntjeh <steyn.ruesink@gmail.com>
2024-10-07 09:11:26 +0200

    wilt niet archiven

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('activities:archive-old')->daily();
+        $schedule->command('activities:archive-old')->hourly();
     }
 
     /**
