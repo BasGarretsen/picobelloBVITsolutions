0714ad0ecba957c79d33d0d84e35da4670d5e4ba
DaanH123 <daan.hendriksen1@student.graafschapcollege.nl>
2024-10-09 15:38:00 +0200

    Set language to dutch

---
+++ resources/lang/nl/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Deze inloggegevens zijn niet bij ons bekend.',
+    'password' => 'Het opgegeven wachtwoord is onjuist.',
+    'throttle' => 'Te veel inlogpogingen. Probeer het opnieuw over :seconds seconden.',
+];
---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Uw wachtwoord is gereset!',
+    'sent' => 'We hebben uw wachtwoord reset link gemaild!',
+    'throttled' => 'Wacht alstublieft voordat u het opnieuw probeert.',
+    'token' => 'Dit wachtwoord reset token is ongeldig.',
+    'user' => "We kunnen geen gebruiker vinden met dat e-mailadres.",
+];
